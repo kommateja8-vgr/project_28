8a4752f000b5af64f11aaf173724bfad9ef47c32
k srikanth <kommateja8@gmail.com>
2026-05-04 16:18:26 +0530

    instialsettings

--- jango_deployee/jango_deployee/start.sh
+++ jango_deployee/jango_deployee/start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-
+python manage.py migrate --noinput
 gunicorn jango_deployee.wsgi:application --bind 0.0.0.0:$PORT
